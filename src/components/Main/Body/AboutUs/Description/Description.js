7695303d8aa4cb4c4d4850e32dd42a260586b971
Abram31 <a447623993@gmail.com>
2023-02-08 15:06:58 +0100

    feat: add

--- src/components/Main/Body/AboutUs/Description/Description.js
+++ src/components/Main/Body/AboutUs/Description/Description.js
@@ -8,7 +8,7 @@ import module from './Description.module.scss'
 export const Description = () => {
     return (
         <div className={module.container}>
-            <img className={module.img} src={`${process.env.PUBLIC_URL}/img/img_about_us.jpg`} alt='' />
+            <img className={module.img} src={`${process.env.PUBLIC_URL}/img/img_about_us_2.jpg`} alt='' />
             <div className={module.wrapper_description}>
                 {aboutUsData.map((data, index) => <Block key={index} value={data.value} />)}
             </div>
